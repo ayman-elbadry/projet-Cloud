b761a03e2fcfd3984970559cb9d2e11f866d49bd
ayman-elbadry <ayman-elbadry@github.local>
2026-03-23 01:20:23 +0100

    Ajout de l'interface graphique animée pour le projet

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/api')
+def api():
     return jsonify({
         "message": "Bienvenue sur l'API du Projet DevOps! (Version automatisée via CI/CD)",
         "status": "success",
